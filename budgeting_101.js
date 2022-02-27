3a3dc59ba2581c95e5da9b38048f2a00b90af37a
Prarthona Paul <paul.prarthona@gmail.com>
2022-02-27 18:06:43 -0500

    Initial commit

---
+++ budgeting_101.js
@@ -0,0 +1,29 @@
+// import {slide as Menu} from 'react-burger-menu'
+import './index.css'; 
+import React from "react";
+
+export default this.props.first {
+    return(
+        <menu>
+            <a className="menu_item" href="/home">
+                Home
+            </a>
+            <a className="menu_item" href="/budget">
+                Budget
+            </a>
+            <a className="menu_item" href="/log_expenses">
+                Log Expenses
+            </a>
+            <a className="menu_item" href="/my_account">
+                My Account
+            </a>
+            <a className="menu_item" href="/help">
+                Help/Contact Us
+            </a>
+            <a className="menu_item" href="/">
+                Budgeting 101
+            </a>
+        </menu>
+    );
+
+};
